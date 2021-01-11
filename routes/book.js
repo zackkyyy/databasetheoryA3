294769f888ec50880fd78c348c476a087967d2ff
Zacky <zacky.kharboutli@fortnox.se>
2021-01-11 19:23:37 +0100

    Small fixes

--- routes/book.js
+++ routes/book.js
@@ -5,7 +5,7 @@ let db = require('../dbParser');
 
 router.get('/createbook', function (req, res) {
 	Promise.all([db.getAllUsers(), db.getAllCategories()]).then(value => {
-		res.render('book/create', {users : value[0], categories: value[1]})
+		res.render('book/create', { users: value[0], categories: value[1] })
 	})
 })
 
@@ -18,16 +18,18 @@ router.post('/book', (request, response) => {
 router.post('/book/delete/:id', (request, response) => {
 	var id = request.params['id']
 	db.deleteBook(id)
-		.then(data =>  response.status(200).redirect('/'))
+		.then(data => response.status(200).redirect('/'))
 })
 
 router.get('/book/:id', function (req, res) {
 	var id = req.params['id']
 	db.getBookById(id).then((data) => {
-		const userId = data[0].author_id;
-		db.getUsersOtherBooks(id, userId).then(books => {
-			res.render('book/show', {book: data, books})
-		})
+		if (data[0]) {
+			const userId = data[0].author_id;
+			db.getUsersOtherBooks(id, userId).then(books => {
+				res.render('book/show', {book: data, books})
+			})
+		}
 	})
 })
 
@@ -36,11 +38,16 @@ router.get('/books', (request, response) => {
 	const queryKeys = Object.keys(request.query);
 	if (queryKeys.length > 0) {
 		Promise.all([db.getFilteredBooks(request.query), db.getAllBooks(), db.getAllCategories(), db.getAllUsersWithBooks()]).then((values) => {
-			response.render('book/booksView', {filteredBooks: values[0], books : values[1], categories: values[2], users: values[3]})
+			response.render('book/booksView', {
+				filteredBooks: values[0],
+				books: values[1],
+				categories: values[2],
+				users: values[3]
+			})
 		})
 	} else {
 		Promise.all([db.getAllBooks(), db.getAllCategories(), db.getAllUsersWithBooks()]).then((values) => {
-			response.render('book/booksView', {books: values[0], categories : values[1], users: values[2]})
+			response.render('book/booksView', { books: values[0], categories: values[1], users: values[2] })
 		})
 	}
 })
@@ -49,7 +56,7 @@ router.get('/book/update/:id', (req, res) => {
 	var id = req.params['id']
 	console.log(id);
 	Promise.all([db.getBookById(id), db.getAllCategories()]).then((values) => {
-		res.render('book/update', {book: values[0], categories : values[1]})
+		res.render('book/update', { book: values[0], categories: values[1] })
 	})
 })
 
@@ -58,7 +65,6 @@ router.post('/book/update/:id', (req, res) => {
 	var id = req.params['id']
 	db.updateBook(id, req).then(res.redirect('/book/' + id));
 })
-
 
 
 router.get('/categories', (request, response) => {
